9540cbc82140d2b21038d036c3a1ffa09a360d23
rida mushtaq <mushtaqrida06@gmail.com>
2024-11-28 20:53:14 +0500

    second push

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,7 @@ module.exports = {
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
-    pathPrefix: "/your-repo-name",
+    pathPrefix: "/SSG-gitsby",
   },
   plugins: [
     `gatsby-plugin-image`,
