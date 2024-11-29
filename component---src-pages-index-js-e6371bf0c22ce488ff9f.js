17c8c94cf25650de673c450a99f382d45c45ba72
rida mushtaq <mushtaqrida06@gmail.com>
2024-11-29 20:24:32 +0500

    Updates

---
+++ component---src-pages-index-js-e6371bf0c22ce488ff9f.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[293],{9639:function(e,t,a){a.r(t);var s=a(6540),l=a(2269);t.default=()=>s.createElement("main",null,s.createElement(l.A,{title:"My Gatsby Site",description:"This is a Gatsby site optimized for SEO"}),s.createElement("h1",null,"Welcome to My Gatsby Site"),s.createElement("p",null,"This is a sample Gatsby site."))}}]);
+//# sourceMappingURL=component---src-pages-index-js-e6371bf0c22ce488ff9f.js.map
