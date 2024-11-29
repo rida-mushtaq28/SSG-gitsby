9f65af249bbb68a93229030495adc152b633340c
rida mushtaq <mushtaqrida06@gmail.com>
2024-11-29 20:04:55 +0500

    third push

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,31 +1,55 @@
-// gatsby-node.js
-const path = require("path")
+const { createFilePath } = require("gatsby-source-filesystem");
+
+exports.createPages = async ({ actions }) => {
+  const { createPage } = actions
+  createPage({
+    path: "/using-dsg",
+    component: require.resolve("./src/templates/using-dsg.js"),
+    context: {},
+    defer: true,
+  })
+}
+
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions;
+
+  // Ensure this applies only to MarkdownRemark nodes
+  if (node.internal.type === "MarkdownRemark") {
+    const slug = createFilePath({ node, getNode, basePath: "posts" }); // Adjust 'posts' to your folder structure
+    createNodeField({
+      node,
+      name: "slug",
+      value: slug,
+    });
+  }
+};
+
+const path = require("path");
 
 exports.createPages = async ({ graphql, actions }) => {
-  const { createPage } = actions
+  const { createPage } = actions;
 
   const result = await graphql(`
     query {
       allMarkdownRemark {
         edges {
           node {
-            id
-            frontmatter {
-              path
+            fields {
+              slug
             }
           }
         }
       }
     }
-  `)
+  `);
 
   result.data.allMarkdownRemark.edges.forEach(({ node }) => {
     createPage({
-      path: node.frontmatter.path, // The path for this blog post (e.g., /blog/my-first-post)
-      component: path.resolve(`src/templates/blog-post.js`), // The template file to use
+      path: node.fields.slug,
+      component: path.resolve("./src/templates/blog-post.js"), // Adjust to your template's location
       context: {
-        id: node.id, // Pass the id of the blog post to the template
+        slug: node.fields.slug,
       },
-    })
-  })
-}
+    });
+  });
+};
